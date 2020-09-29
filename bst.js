ee436259bceaed1ace6bc86e6238bace88166f49
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-29 14:06:39 -0700

    add checkIfBalanced fn

--- bst.js
+++ bst.js
@@ -10,7 +10,7 @@ function createBST(array) {
   return BST;
 }
 
-let numArray = [3, 1, 4, 6, 9, 2, 5, 7];
+let numArray = [3, 1, 4, 6, 9, 2, 5, 7, 8, 9];
 let BST = createBST(numArray);
 // console.log(BST);
 
@@ -40,10 +40,27 @@ function calculateTreeHeight(tree, ticker = 0) {
   }
 }
 
+function calcHeightOfTreeBetter(tree) {
+  if (!tree) {
+    return 0;
+  }
+
+  let leftHeight = 1 + calcHeightOfTreeBetter(tree.left);
+  let rightHeight = 1 + calcHeightOfTreeBetter(tree.right);
+
+  if (leftHeight > rightHeight) {
+    return leftHeight;
+  } else {
+    return rightHeight;
+  }
+}
+
 // console.log(calculateTreeHeight(new BinarySeachTree()));
 // console.log(calculateTreeHeight(newBST));
+// console.log(calcHeightOfTreeBetter(newBST));
+// console.log(calcHeightOfTreeBetter(new BinarySeachTree(9)));
 
-const bigOlBST = createBST([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+const bigOlBST = createBST([0, 1, 2, 3, 4, 5, 6, 7]);
 // console.log(calculateTreeHeight(bigOlBST));
 
 function checkIfIsBST(tree) {
@@ -93,6 +110,67 @@ function createBinaryTree(array) {
 // const anotherBST = createBST([0, 1]);
 // console.log('should be true: ', checkIfIsBST(anotherBST));
 
-function findThirdLargestNode(tree) {
+const justAnotherBST = createBST([1, 0, 2, 3, 2, 3]);
+// console.log(justAnotherBST);
+const simpleBST = createBST([1, 0, 2]);
 
+// k sooooo this one aint workin
+// function findThirdLargest(tree) {
+//   if (!tree.right) {
+//     if (tree.key === tree.parent.key) {}
+//     //return tree.parent.left.key;
+//   }
+//   return findThirdLargest(tree.right);
+// }
+
+// console.log(findThirdLargest(simpleBST));
+// console.log(findThirdLargest(justAnotherBST));
+
+// this one is a little too strict, but almost there
+function checkIfBalanced(tree) {
+  if (tree.right && tree.left) {
+    if (((!tree.right.right && !tree.right.left) && (tree.left.left || tree.left.right)) ||
+      ((!tree.left.left && tree.left.right) && (tree.right.right || tree.right.left))) {
+      return false;
+    }
+    return checkIfBalanced(tree.right) && checkIfBalanced(tree.left);
+  }
+  else if (!tree.right && tree.left) {
+    if (tree.left.right || tree.left.left) {
+      return false;
+    }
+    return checkIfBalanced(tree.left);
+  }
+  else if (!tree.left && tree.right) {
+    if (tree.right.right || tree.right.left) {
+      return false;
+    }
+    return checkIfBalanced(tree.right);
+  }
+  else return true;
 }
+
+console.log('should be false: ', checkIfBalanced(BST));
+console.log('should be false: ', checkIfBalanced(newBST));
+console.log('should be true: ', checkIfBalanced(simpleBST));
+console.log('should be false: ', checkIfBalanced(justAnotherBST));
+console.log('should be true: ', checkIfBalanced(createBST([5, 3, 7, 2, 4, 6, 8])));
+console.log('should be true: ', checkIfBalanced(createBST([5, 3, 7, 2, 4, 6, 8, 9])));
+console.log('should be false: ', checkIfBalanced(createBST([5, 3, 7, 2, 4, 6, 8, 9, 0])));
+
+/*
+function calcHeightOfTreeBetter(tree) {
+  if (!tree) {
+    return 0;
+  }
+
+  let leftHeight = 1 + calcHeightOfTreeBetter(tree.left);
+  let rightHeight = 1 + calcHeightOfTreeBetter(tree.right);
+
+  if (leftHeight > rightHeight) {
+    return leftHeight;
+  } else {
+    return rightHeight;
+  }
+}
+*/
